6eb0e4c79074c1df56832a47178f84f3297758de
Adrian Gallano <adriangallanoengr@gmail.com>
2024-07-18 23:24:20 +0800

    Add filters, search, ordering, and pagination

--- backend/core/views.py
+++ backend/core/views.py
@@ -27,41 +27,149 @@ class ResumeViewSet(ModelViewSet):
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = ["user", "title", "created_at", "updated_at"]
+    search_fields = ["title", "user__username", "created_at", "updated_at"]
+    ordering_fields = ["title", "created_at", "updated_at"]
+    ordering = ["-created_at"]
 
 
 class ResumeTemplatesViewSet(ModelViewSet):
     queryset = ResumeTemplates.objects.all()
     serializer_class = ResumeTemplatesSerializer
     permission_classes = [IsAuthenticated]
-    
-class PersonalInformationViewSet(ModelViewSet): 
-    queryset = PersonalInformation.objects.all() 
+    filter_filelds = ["name", "description"]
+    search_fields = ["name", "description"]
+    ordering_fields = ["name", "description"]
+    ordering = ["name"]
+
+
+class PersonalInformationViewSet(ModelViewSet):
+    queryset = PersonalInformation.objects.all()
     serializer_class = PersonalInformationSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = [
+        "resume",
+        "full_name",
+        "email",
+        "phone_number",
+        "address",
+        "linkedIn",
+        "github",
+        "website",
+    ]
+    search_fields = [
+        "full_name",
+        "email",
+        "phone_number",
+        "address",
+        "linkedIn",
+        "github",
+        "website",
+    ]
+    ordering_fields = [
+        "full_name",
+        "email",
+        "phone_number" "address",
+        "linkedIn",
+        "github",
+        "website",
+    ]
+    ordering = ["full_name"]
 
 
 class EducationViewSet(ModelViewSet):
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = [
+        "resume",
+        "institution",
+        "degree",
+        "field_of_study",
+        "start_date",
+        "end_date",
+        "grade",
+        "description",
+    ]
+    search_fields = [
+        "institution",
+        "degree",
+        "field_of_study",
+        "start_date",
+        "end_date",
+        "grade",
+        "description",
+    ]
+    ordering_fields = [
+        "institution",
+        "degree",
+        "field_of_study",
+        "start_date",
+        "end_date",
+        "grade",
+        "description",
+    ]
+    ordering = ["institution"]
+
 
 class WorkExperienceViewSet(ModelViewSet):
     queryset = WorkExperience.objects.all()
     serializer_class = WorkExperienceSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = [
+        "resume",
+        "company",
+        "position",
+        "start_date",
+        "end_date",
+        "description",
+    ]
+    search_fields = ["company", "position", "start_date", "end_date", "description"]
+    ordering_fields = ["company", "position", "start_date", "end_date", "description"]
+    ordering = ["company"]
+
 
 class SkillsViewSet(ModelViewSet):
     queryset = Skills.objects.all()
     serializer_class = SkillsSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = ["resume", "name", "proficiency"]
+    search_fields = ["name", "proficiency"]
+    ordering_fields = ["name", "proficiency"]
+    ordering = ["name"]
+
 
 class ProjectsViewSet(ModelViewSet):
     queryset = Projects.objects.all()
     serializer_class = ProjectsSerializer
     permission_classes = [IsAuthenticated]
+    filter_fields = ["resume", "title", "description", "link"]
+    search_fields = ["title", "description", "link"]
+    ordering_fields = ["title", "description", "link"]
+    ordering = ["title"]
+
 
 class CertificationsViewSet(ModelViewSet):
     queryset = Certifications.objects.all()
     serializer_class = CertificationsSerializer
     permission_classes = [IsAuthenticated]
-
+    filter_fields = [
+        "resume",
+        "name",
+        "issuing_organization",
+        "issue_date",
+        "expiration_date",
+    ]
+    search_fields = [
+        "name",
+        "issuing_organization",
+        "issue_date",
+        "expiration_date",
+    ]
+    ordering_fields = [
+        "name",
+        "issuing_organization",
+        "issue_date",
+        "expiration_date",
+    ]
+    ordering = ["name"]
--- backend/resume_builder/settings.py
+++ backend/resume_builder/settings.py
@@ -40,12 +40,13 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "corsheaders",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "drf_yasg",
     # Installed
+    "corsheaders",
+    "drf_yasg",
     "rest_framework",
+    'django_filters',
     # Apps
     "core",
 ]
@@ -168,6 +169,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": "10",
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ),
 }
 
