c6f485eff6240cf2cfe81336e34d9fcbada2626e
Adrian Gallano <adriangallanoengr@gmail.com>
2024-07-20 15:43:12 +0800

    Remove support for basic auth

--- backend/resume_builder/settings.py
+++ backend/resume_builder/settings.py
@@ -167,7 +167,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
